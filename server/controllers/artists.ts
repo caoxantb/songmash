6cec661bca2bde7a68eb40f3f15418db3f824ef2
Admin <admin@static.vnpt-hanoi.com.vn>
2022-10-21 12:47:06 +0700

    add axios - connecting client to server

--- server/controllers/artists.ts
+++ server/controllers/artists.ts
@@ -3,6 +3,11 @@ import Artist from "../models/artist";
 
 const artistsRouter = express.Router();
 
+artistsRouter.get("/", async (req: express.Request, res: express.Response) => {
+  const artists = await Artist.find({})
+  res.json(artists)
+})
+
 artistsRouter.post("/", async (req: express.Request, res: express.Response) => {
   const { body } = req;
   const artistCreated = await Artist.create({ ...body });
