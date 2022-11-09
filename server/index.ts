5137647b661e9097032e4bf0bbaaf4da5bb7acdd
ancx <ancx@bravebits.vn>
2022-11-09 18:00:33 +0700

    update port

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,12 @@
 import http from 'http';
 import app from './app';
-import { PORT } from './utils/config';
+import { PORT } from './utils/config';  
 
 // connect to server
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const port = PORT || 8080
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
